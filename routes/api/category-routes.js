3dafefc59f761314bb30b94994d75540b5348187
Glorzo <thisaraeranga@gmail.com>
2022-03-19 00:14:20 -0500

    added Tag routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,7 +5,7 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  
   Category.findAll({
     attributes: ['id', 'category_name'],
     include: [
@@ -24,7 +24,7 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  
   Category.findOne({
     where: {
       id: req.params.id
